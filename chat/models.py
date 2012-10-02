d2956447e20e0f55d7aed20878c4b3e6900dc756
Xabier Larrakoetxea <slok69@gmail.com>
2012-10-02 10:45:16 +0200

    Added basic models in Redis

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,137 @@
-from django.db import models
+import redis
 
-# Create your models here.
+#TODO: Change conection mode!!!
+
+
+class ChatMessage(object):
+    """Class container for the messages"""
+
+    _redis_prefix = "chat:message:%s"
+    _conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    def __init__(self, msg_id, room_name=None):
+
+        self._room = room_name
+        self._msg_id = msg_id
+
+        self._time = None
+        self._user = None
+        self._msg = None
+
+    @property
+    def time(self):
+        return self._time
+
+    @time.setter
+    def time(self, value):
+        self._time = value
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, value):
+        self._user = value
+
+    @property
+    def msg(self):
+        return self._msg
+
+    @msg.setter
+    def msg(self, value):
+        self._msg = value
+
+    @property
+    def room(self):
+        return self._room
+
+    @room.setter
+    def room(self, value):
+        self._room = value
+
+    @property
+    def msg_id(self):
+        return self._msg_id
+
+    @msg_id.setter
+    def msg_id(self, value):
+        self._msg_id = value
+
+    def save(self):
+        # Atomic!
+        pipe = ChatMessage._conn.pipeline()
+
+        #Add the message metadata
+        data = {"room": self._room,
+                "time": self._time,
+                "user": self._user,
+                "msg": self._msg}
+
+        pipe.hmset(ChatMessage._redis_prefix % self._msg_id, data)
+
+        #Add the message to the list (room)
+        redis_room_id = ChatRoom._redis_messages_prefix % self._room
+        pipe.rpush(redis_room_id, self._msg_id)
+        pipe.execute()
+
+    def delete(self):
+        raise NotImplementedError("We don't delete any message *.*")
+
+    @staticmethod
+    def find(msg_id):
+        redis_res = ChatMessage._conn.hgetall(
+                                        ChatMessage._redis_prefix % msg_id)
+        cm = ChatMessage(msg_id, redis_res["room"])
+        cm.msg = redis_res["msg"]
+        cm.time = redis_res["time"]
+        cm.user = redis_res["user"]
+        return cm
+
+
+class ChatRoom(object):
+    """Class that represents the chat room"""
+
+    _redis_users_prefix = "chat:room:%s:users"
+    _redis_messages_prefix = "chat:room:%s:messages"
+    _conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    def __init__(self, name):
+        self._room_id = name
+
+    def messages(self, limit=0):
+        key = ChatRoom._redis_messages_prefix % self._room_id
+        total = self._conn.llen(key)
+
+        #If there is no limit then the start point is the first one
+        start_point = 0
+        if limit != 0:
+            start_point = total - limit
+
+        messages = ChatRoom._conn.lrange(key, start_point, total)
+        message_list = []
+
+        for msg in messages:
+            cm = ChatMessage.find(msg)
+            message_list.append(cm)
+
+        return message_list
+
+    def users(self):
+        key = ChatRoom._redis_users_prefix % self._room_id
+        return ChatRoom._conn.smembers(key)
+
+    @staticmethod
+    def join(room_id, user):
+        key = ChatRoom._redis_users_prefix % room_id
+        ChatRoom._conn.sadd(key, user)
+
+    @staticmethod
+    def connected(room_id, user):
+        key = ChatRoom._redis_users_prefix % room_id
+        return ChatRoom._conn.sismember(key, user)
+
+    @staticmethod
+    def disconnect(room_id, user):
+        key = ChatRoom._redis_users_prefix % room_id
+        ChatRoom._conn.srem(key, user)
--- chat/tests.py
+++ chat/tests.py
@@ -1,16 +1,91 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import uuid
+import time
 
-Replace this with more appropriate tests for your application.
-"""
+from django.test import SimpleTestCase
+import redis
 
-from django.test import TestCase
+from chat.models import ChatMessage, ChatRoom
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ModelTest(SimpleTestCase):
+
+    conn = None
+
+    def setUp(self):
+        ModelTest.conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+    def tearDown(self):
+        ModelTest.conn.flushdb()
+        ModelTest.conn.connection_pool.disconnect()
+
+    def test_chat_message_basic(self):
+        """Tests creation, getters and setter of ChatMessage model"""
+        cm = ChatMessage(uuid.uuid4(), "default")
+        cm.msg = "Python rulz!"
+        cm.user = "slok"
+        cm.time = time.time()
+
+        self.assertEqual(cm.msg, "Python rulz!")
+        self.assertEqual(cm.user, "slok")
+
+    def test_chat_message_save(self):
+        """Tests the save of the instance in redis"""
+        for i in range(100):
+            cm = ChatMessage(uuid.uuid4(), "default")
+            cm.msg = "Python rulz!%d" % i
+            cm.user = "slok%d" % i
+            cm.time = time.time()
+            cm.save()
+
+    def test_chat_message_delete(self):
+        """Tests the delete of the instance in redis"""
+
+        with self.assertRaises(NotImplementedError):
+            cm = ChatMessage(uuid.uuid4(), "default")
+            cm.delete()
+
+    def test_chat_message_find(self):
+        """Tests the find of the instance in redis"""
+
+        uuid_id = uuid.uuid4()
+        cm = ChatMessage(uuid_id, "default")
+        cm.msg = "Python rulz!"
+        cm.user = "slok"
+        cm.time = time.time()
+        cm.save()
+
+        cm_find = ChatMessage.find(uuid_id)
+
+        self.assertEqual(cm_find.msg, cm.msg)
+        self.assertEqual(cm_find.user, cm.user)
+        self.assertEqual(cm_find.room, cm.room)
+
+    def test_chat_messages(self):
+        """Tests the find of the instance in redis"""
+
+        for i in range(30):
+            uuid_id = uuid.uuid4()
+            cm = ChatMessage(uuid_id, "room1")
+            cm.msg = "Python rulz!%d" % i
+            cm.user = "slok%d" % i
+            cm.time = time.time()
+            cm.save()
+
+        cr = ChatRoom("room1")
+        messages = cr.messages(10)
+        self.assertEqual(len(messages), 10)
+
+        name_number = 20
+        for i in messages:
+            self.assertEqual(i.user, "slok%d" % name_number)
+            name_number += 1
+
+    def test_chat_join(self):
+        ChatRoom.join("room1", "slok")
+        self.assertTrue(ChatRoom.connected("room1", "slok"))
+
+    def test_chat_disconnect(self):
+        ChatRoom.join("room1", "slok")
+        self.assertTrue(ChatRoom.connected("room1", "slok"))
+        ChatRoom.disconnect("room1", "slok")
+        self.assertFalse(ChatRoom.connected("room1", "slok"))
